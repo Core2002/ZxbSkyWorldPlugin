150f4af2532aa5821e53fb92036d5b3444a9bd4a
NekokeCore <Core2002@163.com>
2022-06-01 18:24:26 +0800

    优化命名

--- src/main/java/fun/fifu/nekokecore/zxbskyworld/BaseIsLand.java
+++ src/main/java/fun/fifu/nekokecore/zxbskyworld/BaseIsLand.java
@@ -9,12 +9,12 @@ public abstract class BaseIsLand {
     public static int SIDE = 1024;
     public static int MAX_SKY_LOC = 29296;
 
-    public static int getRRForm(int SkyX) {
-        return SIDE * SkyX;
+    public static int getRRForm(int SkyR) {
+        return SIDE * SkyR;
     }
 
-    public static int getRREnd(int SkyX) {
-        return (SIDE * (SkyX + 1)) - 1;
+    public static int getRREnd(int SkyR) {
+        return (SIDE * (SkyR + 1)) - 1;
     }
 
 }
